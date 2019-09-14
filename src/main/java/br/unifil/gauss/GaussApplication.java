4e885b49539042dac93e9d42bcfe5a9aaef34cef
brunomorais <bruno.morais@edu.unifil.br>
2019-09-13 21:16:10 -0300

    Commit inicial

---
+++ src/main/java/br/unifil/gauss/GaussApplication.java
@@ -0,0 +1,13 @@
+package br.unifil.gauss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaussApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaussApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/unifil/gauss/GaussApplicationTests.java
@@ -0,0 +1,16 @@
+package br.unifil.gauss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaussApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
